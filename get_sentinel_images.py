63b97f77aa70e5695f4925976789486d772aac53
Tim <35193581+tonzowonzo@users.noreply.github.com>
2019-08-25 21:12:56 +0200

    added iteration of downloading

--- get_sentinel_images.py
+++ get_sentinel_images.py
@@ -3,6 +3,11 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from sentinelsat import SentinelAPI
+import rasterio
+from rasterio.plot import show
+import shapely
+from shapely.geometry import box
+
 os.getcwd()
 # Read in geology csv.
 df = pd.read_csv("data/geological/ofr20051294/deposit.csv")
@@ -37,17 +42,36 @@ df = pd.read_csv("geology_footprints.csv")
 df.set_index("Unnamed: 0", inplace=True)
 footprint = df["wkt"][0]
 
-footprint = f"POINT ({df['longitude'][0]} {df['latitude'][0]})"
 api = SentinelAPI(username, password, 'https://scihub.copernicus.eu/dhus')
-# search by polygon, time, and SciHub query keywords
-products = api.query(footprint,
-                     platformname='Sentinel-2',
-                     area_relation='Intersects',
-                     cloudcoverpercentage=(0, 10))
 
-products_df = api.to_dataframe(products)
+for i in df.index:
+    footprint = f"POINT ({df['longitude'][i]} {df['latitude'][i]})"
+    
+    # search by polygon, time, and SciHub query keywords
+    products = api.query(footprint,
+                         platformname='Sentinel-2',
+                         area_relation='Intersects',
+                         cloudcoverpercentage=(0, 5))
+    
+    products_df = api.to_dataframe(products)
+    
+    # sort and limit to first 5 sorted products
+    products_df_sorted = products_df.sort_values(['cloudcoverpercentage', 'ingestiondate'], ascending=[True, True])
+    api.download(products_df_sorted.index[0])
 
-# sort and limit to first 5 sorted products
-products_df_sorted = products_df.sort_values(['cloudcoverpercentage', 'ingestiondate'], ascending=[True, True])
-api.download(products_df_sorted.index[0])
-
+    # Clip the images
+    img_name = products_df_sorted.title[0].split(".")[0] 
+    for img_loc in os.listdir(f"data/satellite/" + img_name + ".SAFE" + "/GRANULE/"):
+        for img in os.listdir(f"data/satellite/" + img_name + ".SAFE" + "/GRANULE/" +
+                              img_loc + "/IMG_DATA/"):
+            output_location = f"data/satellite/clipped_{df["]}"
+            # Load in image to be clipped.
+            data = rasterio.open(f"data/satellite/" + img_name + ".SAFE" + "/GRANULE/" +
+                              img_loc + "/IMG_DATA/" + img)
+            show((data, 1), cmap='terrain')
+            # Create bounding box for clipping image.
+            minx, miny = df["lower left lat"][i], df["lower left long"][i]
+            maxx, maxy = df["upper right lat"][i], df["upper right long"][i]
+            bbox = box(minx, miny, maxx, maxy)
+            
+            
