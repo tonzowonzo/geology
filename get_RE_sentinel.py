1ed134f0de60dc46c3b6da3a5ae214b79b491092
Tim <35193581+tonzowonzo@users.noreply.github.com>
2019-08-26 23:52:17 +0200

    Add files via upload

---
+++ get_RE_sentinel.py
@@ -0,0 +1,12 @@
+# Import libraries.
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from sentinelsat import SentinelAPI
+import rasterio
+from rasterio.plot import show
+import shapely
+from shapely.geometry import box
+from rasterio.enums import Resampling
+
+df = pd.read_csv("USGS_REE_US_CSV/Loc_Pt.csv")
